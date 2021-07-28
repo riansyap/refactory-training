49b68f7f79dd24873f443ff294c310848fe4b8a8
RiansyaP <riansya.pamusti@esdm.go.id>
2021-07-28 12:27:24 +0700

    add Numerica

--- Day3-PHP-OOP/src/Numerica.php
+++ Day3-PHP-OOP/src/Numerica.php
@@ -4,5 +4,101 @@ namespace App;
 
 class Numerica
 {
+    public array $number;
 
+    public function __construct($number){
+        $this->number=$number;
+    }
+
+    public function first(){
+        return $this->number[0];
+    }
+    
+    public function last(){
+        return $this->number[count($this->number)-1];
+    }
+
+    public function min(){
+        return min($this->number);
+    }
+
+    public function max(){
+        return max($this->number);
+    }
+
+    public function sum(){
+        return array_sum($this->number);
+    }
+
+    public function rsort(){
+        $sorted=$this->number;
+        rsort($sorted);
+        return $sorted;
+    }
+
+    public function sort(){
+        $sorted=$this->number;
+        sort($sorted);
+        return $sorted;
+    }
+
+    public function odd(){
+        $temps=$this->number;
+        foreach($temps as $key => $value){
+            if($value %2==0){
+                unset($temps[$key]);
+            }
+        }
+        return $temps;
+    }
+
+    public function even(){
+        $temps=$this->number;
+        foreach($temps as $key => $value){
+            if($value %2!=0){
+                unset($temps[$key]);
+            }
+        }
+        return $temps;
+    }
+
+    public function greaterThan($pivot){
+        $temps=$this->number;
+        foreach($temps as $key => $value){
+            if($value <= $pivot){
+                unset($temps[$key]);
+            }
+        }
+        return $temps;
+    }
+
+    public function greaterThanEqual($pivot){
+        $temps=$this->number;
+        foreach($temps as $key => $value){
+            if($value < $pivot){
+                unset($temps[$key]);
+            }
+        }
+        return $temps;
+    }
+
+    public function lessThan($pivot){
+        $temps=$this->number;
+        foreach($temps as $key => $value){
+            if($value >= $pivot){
+                unset($temps[$key]);
+            }
+        }
+        return $temps;
+    }
+
+    public function lessThanEqual($pivot){
+        $temps=$this->number;
+        foreach($temps as $key => $value){
+            if($value > $pivot){
+                unset($temps[$key]);
+            }
+        }
+        return $temps;
+    }
 }
